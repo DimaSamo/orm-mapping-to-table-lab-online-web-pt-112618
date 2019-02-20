3758e99bdc2bf2ad59b41032df415586d058cde0
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 04:35:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,4 +28,11 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name,grade) VALUES (?, ?)
+    SQL
+    db[:conn].execute(sql,self.name,self.grade)
+  end
+
 end
